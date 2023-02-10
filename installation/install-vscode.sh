b68c1820d12cec04205399cf87990badd6af8e35
Zuhrianda <zuhrianda@NB-Zuhrianda-DS.local>
2023-02-10 09:54:15 +0700

    add install vscode

---
+++ installation/install-vscode.sh
@@ -0,0 +1,4 @@
+# installation for vscode for mac apple chip
+wget https://az764295.vo.msecnd.net/stable/441438abd1ac652551dbe4d408dfcec8a499b8bf/VSCode-darwin-arm64.zip -O vscode-arm64.zip
+unzip vscode-arm64.zip
+mv Visual Studio Code.app /Applications
